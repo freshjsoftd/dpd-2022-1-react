636a00997554e6305722a3e88ae3ff13d07b3d18
yan <freshjs.oftd@gmail.com>
2022-05-22 14:03:19 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/main/Main';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +21,9 @@ function App() {
         </a>
       </header>
     </div>
+    <Main main="React" app="App"/>
+    </>
+    
   );
 }
 
---
+++ src/components/main/Main.js
@@ -0,0 +1,18 @@
+import {Component} from 'react';
+import './main.css'
+
+
+class Main extends Component {
+
+  render(){
+    const {main} = this.props;
+    console.log(main)
+    return (
+      <>
+        <h1>It is main {this.props.main} component</h1>
+      </>
+    )
+  }
+}
+
+export default Main;
