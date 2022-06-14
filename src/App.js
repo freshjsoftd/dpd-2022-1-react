68c3df1cc15bd15dfab331370a2f4c22c86e1cb8
yan <freshjs.oftd@gmail.com>
2022-06-14 11:56:33 +0300

    Install json-server

--- src/App.js
+++ src/App.js
@@ -1,71 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import WatchForm from './components/WatchForm/WatchForm'
-import WatchList from './components/WatchList/WatchList'
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import WatchForm from './components/WatchForm/WatchForm';
+import WatchList from './components/WatchList/WatchList';
 import initialState from './model/initial-to-watch';
 
-
-
 function App() {
- 
-    const [toWatchMovies, setToWatchMovies] =  useState([]);
+  const [toWatchMovies, setToWatchMovies] = useState(initialState);
 
-    useEffect(() => {
-      const movies = JSON.parse(localStorage.getItem('movies'));
-      if(!movies){
-        setToWatchMovies(initialState);
-      }else{
-        setToWatchMovies(movies);
-      }
-        }, [])
+  useEffect(() => {
+    getFromStorage();
+  }, []);
 
-    function deleteToWatch(id) {
-      const newWatchMovies = toWatchMovies.filter((movie) => movie.id !== id);
-      setToWatchMovies(newWatchMovies);
-      saveToStorage(newWatchMovies);
+  function deleteToWatch(id) {
+    const newWatchMovies = toWatchMovies.filter((movie) => movie.id !== id);
+    setToWatchMovies(newWatchMovies);
+    saveToStorage(newWatchMovies);
+  }
+
+  function saveToStorage(movies) {
+    localStorage.setItem('movies', JSON.stringify(movies));
+  }
+
+  function getFromStorage() {
+    const movies = JSON.parse(localStorage.getItem('movies'));
+    if (!movies) {
+      setToWatchMovies(initialState);
+    } else {
+      setToWatchMovies(movies);
     }
+  }
 
-    function saveToStorage(movies){
-      localStorage.setItem('movies', JSON.stringify(movies));
-    }
+  function toggleToWatch(id) {
+    const newWatchMovies = toWatchMovies.map((movie) =>
+      movie.id !== id ? movie : {...movie, isDone: !movie.isDone},
+    );
+    setToWatchMovies(newWatchMovies);
+    saveToStorage(newWatchMovies);
+  }
 
-    // function getFromStorage(){
-    //   const movies = JSON.parse(localStorage.getItem('movies'));
-    //   if(!movies){
-    //     setToWatchMovies(initialState);
-    //   }else{
-    //     setToWatchMovies(saveToStorage(movies));
-    //   }
-      
-    // }
-
-    function toggleToWatch(id) {
-      const newWatchMovies = toWatchMovies.map((movie) => movie.id !== id ? movie 
-                                      :  {...movie, isDone: !movie.isDone});
-      setToWatchMovies(newWatchMovies);
-      saveToStorage(newWatchMovies);
-    }
-
-    function addNewToWatch(toWatch){
-      toWatch.id = Date.now();
-      const newWatchMovies = [...toWatchMovies, toWatch];
-      setToWatchMovies(newWatchMovies);
-      saveToStorage(newWatchMovies);
-    }
+  function addNewToWatch(toWatch) {
+    toWatch.id = Date.now();
+    const newWatchMovies = [...toWatchMovies, toWatch];
+    setToWatchMovies(newWatchMovies);
+    saveToStorage(newWatchMovies);
+  }
 
   return (
     <div className="container">
-        <WatchList 
-          movies={toWatchMovies}
-          onToggle={toggleToWatch}
-          onDelete={deleteToWatch}
-          />
-        <WatchForm 
-          onSubmit={addNewToWatch}
-        //             onSave={this.saveToWatch}
-                    />
+      <WatchList
+        movies={toWatchMovies}
+        onToggle={toggleToWatch}
+        onDelete={deleteToWatch}
+      />
+      <WatchForm
+        onSubmit={addNewToWatch}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/WatchForm/WatchForm.js
+++ src/components/WatchForm/WatchForm.js
@@ -1,52 +1,46 @@
-import React, { useState } from 'react'
-import './WatchForm.css'
+import React, {useState} from 'react';
+import './WatchForm.css';
 
 function WatchForm({onSubmit}) {
-
   const [title, setTitle] = useState('');
   const [director, setDirector] = useState('');
 
-  function onIputChange(e){
-    if(e.target.name === 'title'){
+  function onIputChange(e) {
+    if (e.target.name === 'title') {
       setTitle(e.target.value);
     }
-    if(e.target.name === 'director'){
+    if (e.target.name === 'director') {
       setDirector(e.target.value);
     }
   }
 
-  function onFormSubmit(e){
+  function onFormSubmit(e) {
     e.preventDefault();
-    onSubmit(
-      {
-        title: title,
-        director: director,
-        isDone: false,
-    }
-    )
+    onSubmit({
+      title: title,
+      director: director,
+      isDone: false,
+    });
+    setTitle('');
+    setDirector('');
   }
 
   return (
-    <form className="watch-form"
-          onSubmit={onFormSubmit}
-          >
-        <input type="text"
-              name='title'
-              value={title}
-              onChange={onIputChange}
-              />
-        <input type="text"
-              name='director'
-              value={director}
-              onChange={onIputChange}
-              />
-        <button className="add">Add</button>
-        {/* <button 
+    <form className="watch-form" onSubmit={onFormSubmit}>
+      <input type="text" name="title" value={title} onChange={onIputChange} />
+      <input
+        type="text"
+        name="director"
+        value={director}
+        onChange={onIputChange}
+      />
+      <button className="add">Add</button>
+      {/* <button 
               className="save"
               type='button'
               onClick={this.props.onSave}>Save</button> */}
     </form>
-  )
+  );
 }
 
-export default WatchForm
+export default WatchForm;
